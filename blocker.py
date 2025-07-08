f436099b671b154a1d030af8d13706c4887b76fb
UsmanCh1226 <usmanch@umich.edu>
2025-07-08 19:32:16 -0400

    Creating block/unblock functions

---
+++ blocker.py
@@ -0,0 +1,62 @@
+import time
+import platform
+import os
+import sys
+
+
+HOSTS_PATH = r"C:\Windows\System32\drivers\etc\hosts" if os.name == 'nt' else "/etc/hosts"
+REDIRECT_IP = "127.0.0.1"
+BLOCK_TAG = "# Blocked by FocusBlocker"
+
+WEBSITES= ["www.youtube.com",
+                     "facebook.com",
+                     "www.netflix.com",
+                     "netflix.com"]
+
+
+
+def blocking_websites():
+    print("Blocking websites...")
+    with open(HOSTS_PATH, "r+") as file:
+        content = file.read()
+        file.seek(0)
+        already_blocked = False
+        for line in content:
+            if any(site in line for site in WEBSITES):
+                already_blocked = True
+                break
+
+        if already_blocked:
+            print("Websites are already blocked.")
+
+        else:
+            file.writelines(content)
+            for website in WEBSITES:
+                file.write(f"{REDIRECT_IP} {website} {BLOCK_TAG}\n")
+            print("Websites successfully blocked.")
+
+
+def unblocking_websites():
+    with open(HOSTS_PATH, "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
+        for line in lines:
+            if BLOCK_TAG not in line:
+                file.write(line)
+        file.truncate()
+    print("Websites successfully unblocked.")
+
+
+
+
+if __name__ =="__main__":
+    action = input("Type 'block' to block or 'unblock' to unblock websites: ").strip()
+    if action == 'block':
+        blocking_websites()
+    elif action = 'unblock':
+        unblocking_websites()
+    else:
+        print("Invalid action.")
+
+
+
